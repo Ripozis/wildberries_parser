6b0b3c4892243fa9c82522efc1c0296263c95dca
Илья <41539147+Ripozis@users.noreply.github.com>
2024-03-30 19:52:32 +0300

    Запрос через прокси и парсинг первой страницы категории

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
+# -*- coding: utf-8 -*-
 # Стартовый файл для теста git
+
+import requests
+import json
+from query_settings import url, proxies, headers # импортируем настройки запроса. 
+prs_json = r'C:/Users/Илья/Desktop/wildberries_parser/wildberries_parser/data.json'
+
+def main(url, proxies, headers): #url, proxies, headers
+    response = requests.post(url, proxies=proxies, headers=headers)
+    
+    
+    print('Response HTTP Status Code: ', response.status_code)
+    #полученые данные переводим в json
+    response_json = response.json()
+    # полученные данные сохранить в json файл
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(response.json(), f, ensure_ascii=False, indent=4)
+
+    # читаем json файл data.json' и выводим в цикле все даныные из "data": {"products"
+    # with open(prs_json, encoding='utf-8') as f:
+    #     data = (json.load(f))['data']['products']
+    #     for item in data: 
+    #         print('----------------------------------', end='\n')
+    #         article_id = item['id']
+    #         brand = item['brand']
+    #         name = item['name']
+    #         price = item['sizes'][0]['price']['total']
+    #         print(f'Бренд: {item["brand"]}')
+    #         print(f'Название: {item["name"]}')
+    #         print(f'Артикул: {item["id"]}')
+    #         print(f'цена: {price}')
+    data = (response_json)['data']['products']
+    for item in data:
+            
+            print('----------------------------------', end='\n')
+            
+            article_id = item['id']
+            brand = item['brand']
+            name = item['name']
+            price = item['sizes'][0]['price']['total']
+
+
+            print(f'Бренд: {item["brand"]}')
+            print(f'Название: {item["name"]}')
+            print(f'Артикул: {item["id"]}')
+            print(f'цена: {price}')
+            
+if __name__ == '__main__':
+    main(url, proxies, headers) #url, proxies, headers
---
+++ query_settings.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+url = 'https://catalog.wb.ru/catalog/electronic22/v2/catalog?appType=1&curr=rub&dest=12358048&sort=popular&spp=30&subject=515&uclusters=1'
+
+proxies = {
+            'http': '185.117.154.164:80'
+            }
+
+headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Connection": "keep-alive",
+    "Host": "catalog.wb.ru",
+    "Origin": "https://www.wildberries.ru",
+    "Referer": "https://www.wildberries.ru/catalog/elektronika/smartfony-i-telefony/vse-smartfony",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "cross-site",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "authorization": "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTE3MzcyNzAsInZlcnNpb24iOjIsInVzZXIiOiIxMzczNjY3NzkiLCJzaGFyZF9rZXkiOiIxNCIsImNsaWVudF9pZCI6IndiIiwic2Vzc2lvbl9pZCI6ImQ5OTAyODk1YTg3MDRjZjY4OTk3ZDUwYzU2ZGE4ZmEwIiwidXNlcl9yZWdpc3RyYXRpb25fZHQiOjE3MDI2NTczMDQsInZhbGlkYXRpb25fa2V5IjoiNGVmZWU4NWRmMWY5ZjJkNzg3ZTdmOWEyN2JiNzhmYWQwMGZiODRiZmMwNGU2MjZjNDM1M2JiZDVhZDMyNjU0NyIsInBob25lIjoiNG9NWE9QWVE3RmZPYnVOZHZsNDdjdz09In0.G0Tm519hYhZLWknjd83KC6yiGAINumJJerEaq4rtc8G9-_8QhOBMFXZMOysX7OZcgfmCQlU3O_lw423YH_SoGd2Iy-q7mXZHnQXCpmUVj4z-sGxGrCo2pk3B_TzxWU6m2BC390_Gb5q28A5S8lXsDWEuSojhZqWlB39jyJFTa7MieEBOFLcDOk4moTYjrKb_CYjr3UjPNUOC-UD8MkdOVX51SnUM1JkGLcIohXuWeJ8usdvgMQ_cFgs9kaFvzupzfYzwCxbf1keAei6-6S8eDMz-IBFUktw0mTNG8TH5AIWFRvhX6Onnyq10DqfwYvV5q17pcb799-hJNjiHlXePHQ",
+    "dnt": "1",
+    "sec-ch-ua": '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-gpc": "1"
+}
